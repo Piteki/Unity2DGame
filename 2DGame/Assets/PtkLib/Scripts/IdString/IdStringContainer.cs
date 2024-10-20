6248f49d47c7e2620ff8064cc95ce53b7a5e6b77
Piteki <57817383+Piteki@users.noreply.github.com>
2024-10-20 18:02:03 +0900

    Container 作成中

---
+++ 2DGame/Assets/PtkLib/Scripts/IdString/IdStringContainer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Ptk.IdStrings
+{
+	/// <summary>
+	/// IdString Container
+	/// </summary>
+	/// <remarks>
+	/// 複数の IdString を格納するクラス
+	/// </remarks>
+	public class IdStringContainer
+	{
+		private List< IdString > mList;
+
+
+	}
+}
